792ecc4f6d627aead3edb9e99dc2819c4207ccc4
zhixi.liu <zhixi.liu@baifendian.com>
2018-10-24 16:06:15 +0800

    init env

---
+++ app/index.js
@@ -0,0 +1,18 @@
+const Koa = require("koa");
+
+const config = require("./config");
+const frame = require("./z-frame");
+const socket = require("./socket");
+const router = require("./routers");
+const { showBanner } = require("./utils/banner");
+
+const app = new Koa();
+showBanner(config.appName, config.version);
+
+frame.attach(app, config);
+socket.attach(app);
+app.use(router.routes());
+
+app.listen(config.port, () => {
+  console.log(`${config.env} server started on port ${config.port}`);
+});
